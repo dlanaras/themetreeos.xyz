8c506f8201aa2b3f2be99f9785dc7121fe1da8ba
dlanaras <dimitrislanaras04@outlook.com>
2022-06-02 20:01:50 +0200

    about me content + desing improvements

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -9,7 +9,17 @@ export class AboutmeComponent implements OnInit {
 
   constructor() { }
 
+  height : number = 0;
+  width : number = 0;
+
   ngOnInit(): void {
+    this.height = window.innerHeight;
+    this.width = window.innerWidth;
+  }
+
+  calcSize(event : Event): void {
+    this.height = ((event.target) as Window).innerHeight;
+    this.width = ((event.target) as Window).innerWidth;
   }
 
 }
